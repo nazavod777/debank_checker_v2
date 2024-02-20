0d7b55be0a5b8b5ec17118d37e11899dfafa3b76
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-02-20 21:38:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,33 +33,39 @@ if __name__ == '__main__':
     if not exists(path='results'):
         mkdir(path='results')
 
-    with open(file='data/accounts.txt',
-              mode='r',
-              encoding='utf-8-sig') as file:
-        accounts_list: list[str | FormattedAccount] = [row.strip().rstrip() for row in file]
-        accounts_list: list[str | FormattedAccount] = list(set(accounts_list))
-        accounts_list: list[FormattedAccount] = [format_account(account_data=row) for row in accounts_list]
-
-    accounts_list: list[FormattedAccount] = [current_account for current_account in accounts_list if current_account]
-
     with open(file='data/proxies.txt',
               mode='r',
               encoding='utf-8-sig') as file:
         proxy_count: int = len([row.strip() for row in file if row.strip()])
 
-    logger.success(f'Successfully Loaded {len(accounts_list)} Accounts / {proxy_count} Proxies')
-    threads: int = int(input('\nThreads: '))
+    logger.success(f'Successfully Loaded {proxy_count} Proxies')
 
-    print()
-    loader.semaphore = asyncio.Semaphore(value=threads)
+    with open(file='data/accounts.txt',
+              mode='r',
+              encoding='utf-8-sig') as file:
+        while True:
+            data = file.read(16768)
 
-    try:
-        import uvloop
+            if not data:
+                break
 
-        uvloop.run(main())
+            accounts_list: list[str | FormattedAccount] = [row.strip().rstrip() for row in file]
+            accounts_list: list[str | FormattedAccount] = list(set(accounts_list))
+            accounts_list: list[FormattedAccount] = [format_account(account_data=row) for row in accounts_list]
+            accounts_list: list[FormattedAccount] = [current_account for current_account in accounts_list if
+                                                     current_account]
 
-    except ModuleNotFoundError:
-        asyncio.run(main())
+            loader.semaphore = asyncio.Semaphore(value=len(accounts_list))
+
+            logger.info(f'Loaded Accounts: {len(accounts_list)}')
+
+            try:
+                import uvloop
+
+                uvloop.run(main())
+
+            except ModuleNotFoundError:
+                asyncio.run(main())
 
     logger.success('Work has been successfully completed')
     input('\nPress Enter to Exit..')
